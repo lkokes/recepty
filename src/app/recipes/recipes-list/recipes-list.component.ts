b4c8a0f25610c6c679826e98481c98487995e290
Lucia Kokesova <lkokesova@gmail.com>
2022-01-18 14:15:34 +0100

    Display selected  recipe details

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,14 +7,21 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipes-list.component.css']
 })
 export class RecipesListComponent implements OnInit {
+  
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+
   recipes: Recipe[] = [
     new Recipe('názov', 'popis receptu', 'assets/images/recept-kura.jpg'),
-    new Recipe('názov', 'popis receptu', 'assets/images/recept-kura.jpg')
+    new Recipe('Iny názov', 'popis receptu', 'assets/images/recept-kura.jpg')
   ];
 
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  onRecipeSelected(recipe: Recipe){
+    this.recipeWasSelected.emit(recipe);
+  };
 
 }
